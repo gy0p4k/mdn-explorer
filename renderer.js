125a120e8b94c034693f2f94d0cd9f75dd474176
gy0p4k <gyorgypakozdi@gmail.com>
2018-08-07 09:37:12 +0800

    fix(main): refactor, delete comments

--- renderer.js
+++ renderer.js
@@ -1,10 +1,5 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
 const scraper = require('google-search-scraper');
 const resultContainer = document.querySelector('.result');
-
-console.log("hello")
 
 const search = () => {
     let query = document.querySelector('.searchbar').value
