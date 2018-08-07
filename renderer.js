e6c6ef4e7260ff51d1ee2427873099ebb92418c9
gy0p4k <gyorgypakozdi@gmail.com>
2018-08-07 10:50:31 +0800

    feat(js): new dep, queries are fined

--- renderer.js
+++ renderer.js
@@ -2,33 +2,34 @@ const scraper = require('google-search-scraper');
 const resultContainer = document.querySelector('.result');
 
 const search = () => {
-    let query = document.querySelector('.searchbar').value
-    scraper.search({query: query, limit: 10}, (err, url, meta) => {
+    let query = document.querySelector('.searchbar').value + 'site:developer.mozilla.org';
+    resultContainer.innerHTML = '';
+    scraper.search({query: query, limit: 10}, function(err, url, meta) {
         if(err) throw err;
         handleSearch(url, meta)
       });
 }
 
 const handleSearch = (url, meta) => {
-    if (resultContainer.childNodes.length > 10) {
-        resultContainer.innerHTML = '';
-    } else {
-        addResult(meta)
-    }
+    addResult(url, meta)
 }
 
+const resultClickHandler = (event) => {
+    console.log(event.target.classList[1]);
+}
 
-const addResult = (meta) => {
+const addResult = (url, meta) => {
     let result = document.createElement('div');
     let title = document.createElement('a');
     let description = document.createElement('p');
     title.innerText = meta.title;
     title.classList = 'title'
     description.innerText = meta.desc;
-    description.classList = 'desc'
+    description.classList = 'desc ' + url;
     result.appendChild(title);
     result.appendChild(description);
-    result.classList = 'result';
+    result.classList = 'result ' + url;
+    result.addEventListener('click', resultClickHandler)
     resultContainer.appendChild(result)
 }
 
